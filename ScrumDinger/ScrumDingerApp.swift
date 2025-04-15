cbce66cf11ef3765aee74d90a663ac3a0edc8bde
katherinehambley <superdesigngirl@outlook.com>
2025-04-15 10:30:41 -0500

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Katherine Hambley on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
