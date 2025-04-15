648d6e6e9f0195da8c7610cefc2f9c91a0665dd0
katherinehambley <superdesigngirl@outlook.com>
2025-04-15 13:14:15 -0500

    Initial project files

---
+++ ScrumDinger/MeetingView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ScrumDinger
+//
+//  Created by Katherine Hambley on 4/15/25.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        ProgressView(value: 10, total: 15)
+    }
+}
+
+#Preview {
+    MeetingView()
+}
