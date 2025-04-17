c5df35e952b4fb86333645e7c0c34442dbb4c277
katherinehambley <superdesigngirl@outlook.com>
2025-04-17 08:21:18 -0500

    Added ScrumsView and added to root

---
+++ ScrumDinger/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  ScrumDinger
+//
+//  Created by Katherine Hambley on 4/16/25.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
