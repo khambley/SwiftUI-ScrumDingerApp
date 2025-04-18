60a23e27385ba26d1e2c74c985f147372d1f7d26
katherinehambley <superdesigngirl@outlook.com>
2025-04-18 11:36:43 -0500

    Added DetailView

---
+++ ScrumDinger/DetailView.swift
@@ -0,0 +1,33 @@
+//
+//  DetailView.swift
+//  ScrumDinger
+//
+//  Created by Katherine Hambley on 4/17/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack{
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) min")
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
